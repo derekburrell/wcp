ba5baba4b325597b637f6aebdb18f11ffea26aa2
derekburrell <derekburrell@users.noreply.github.com>
2017-11-21 01:16:52 +0100

    Update proxy.go

--- proxy.go
+++ proxy.go
@@ -16,7 +16,7 @@ import (
 	"golang.org/x/net/websocket"
 )
 
-const basePkg = "github.com/derekburrell/webcamproxy"
+const basePkg = "github.com/derekburrell/wcp"
 
 // --------------------------
 
